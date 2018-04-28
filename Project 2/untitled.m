0d659320ddbd4c1b92509fcdd8c569bca5eec808
Erik <jansson.grennberg.erik@gmail.com>
2018-04-28 12:25:55 +0200

    alla våra problem är lösta!
    Nej

--- Project 2/untitled.m
+++ Project 2/untitled.m
@@ -7,14 +7,15 @@ autocorr(returns,20); figure(2)
 parcorr(returns,20); 
 [~,pVal]=lbqtest(returns,20);
 %% uppgift 2
+maxNoPar=5;
 numberOfObservations=length(returns);
-loglikliMatrix = zeros(10,10); % Initialize
-pqMatrix = zeros(10,10); %to save number of paramaters
-for q=1:10
-  for p=1:10
+loglikliMatrix = zeros(maxNoPar); % Initialize
+pqMatrix = zeros(maxNoPar); %to save number of paramaters
+for q=1:maxNoPar
+  for p=1:maxNoPar
     try
       mod = arima(p,0,q); %model struct
-      [~,~,logL,~] = estimate(mod,returns,'Display','off','Constant0',0); %fits model
+      [fit,~,logL,~] = estimate(mod,returns,'Display','off'); %fits model
       loglikliMatrix(p,q) = logL; %save in loglik matrix
       pqMatrix(p,q) = 1+p+q; %save number of parameters
     catch
@@ -26,7 +27,42 @@ for q=1:10
 end
 
 %%
-[~,bic] = aicbic(reshape(loglikliMatrix,100,1),reshape(pqMatrix,100,1),numberOfObservations);
+[~,bic] = aicbic(reshape(loglikliMatrix,maxNoPar^2,1),reshape(pqMatrix,maxNoPar^2,1),numberOfObservations);
 [value,index]=min(bic);
-[i,j]=ind2sub([10 10],34)
-    
+[pOpt,qOpt]=ind2sub([ maxNoPar],index);
+%% TASK 3
+modOpt=arima(pOpt,0,qOpt);
+modEstOpt=estimate(modOpt,returns,'Display','off'); %osäker op detta
+[residuals,condVar]=infer(modEstOpt,returns); %maybe we shall standardize the residuals? 
+%
+figure(1);clf;
+plot(residuals);
+figure(2);clf
+autocorr(residuals,20);
+figure(3);clf
+qqplot(residuals);
+%  to heavy tails? Hetroscedacity (it seems)!, linear dependecies? 
+%% squared residuals
+figure(2);clf; 
+autocorr(residuals.^2,20);
+%we must allow for hetroscacity and whale-tails! 
+%% TASK4
+maxNoPar=5;
+numberOfObservations=length(returns);
+loglikliMatrix = zeros(maxNoPar); % Initialize
+pqMatrix = zeros(maxNoPar); %to save number of paramaters
+for q=1:maxNoPar
+  for p=1:maxNoPar
+    try
+      mod = garch(p,q); %model struct
+      [fit,~,logL,~] = estimate(mod,returns,'Display','off','); %fits model
+      loglikliMatrix(p,q) = logL; %save in loglik matrix
+      pqMatrix(p,q) = 1+p+q; %save number of parameters
+    catch
+      disp("warning")
+      loglikliMatrix(p,q)=NaN;
+      pqMatrix(p,q)=NaN;
+    end
+  end
+end
+
