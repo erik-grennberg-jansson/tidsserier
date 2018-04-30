a354a000636dc0c503f8da6bfc9c3a02287a61a0
Jacob Lindbäck <lindbackjacob@gmail.com>
2018-04-30 17:56:44 +0200

    Some minor changes of variable names. Further, diagnostics in task 4 is now included

--- Project 2/untitled.m
+++ Project 2/untitled.m
@@ -1,37 +1,39 @@
-%% uppgift 1
-load("sp500.mat"); %load data
-plot(sp500_returns); %plots are nice
+%% Task 1
+load('sp500.mat'); %load data
 returns=sp500_returns-mean(sp500_returns); %mean correct 
-figure(1)
-autocorr(returns,20); figure(2) 
+plot(returns); %plots are nice
+figure(2)
+autocorr(returns,20); figure(3) 
 parcorr(returns,20); 
 [~,pVal]=lbqtest(returns,20);
-%% uppgift 2
-maxNoPar=5;
-numberOfObservations=length(returns);
-loglikliMatrix = zeros(maxNoPar); % Initialize
-pqMatrix = zeros(maxNoPar); %to save number of paramaters
-for q=1:maxNoPar
-  for p=1:maxNoPar
+%% Task 2
+num_params=5;
+num_obs=length(returns);
+loglikelihood = zeros(num_params); % Initialize
+pqMatrix = zeros(num_params); %to save number of paramaters
+for q=1:num_params
+  for p=1:num_params
     try
-      mod = arima(p,0,q); %model struct
-      [fit,~,logL,~] = estimate(mod,returns,'Display','off'); %fits model
-      loglikliMatrix(p,q) = logL; %save in loglik matrix
+      model = arima(p,0,q); %model struct
+      model.Constant = 0;
+      [~,~,logL,~] = estimate(model,returns,'Display','off'); %fits model
+      loglikelihood(p,q) = logL; %save in loglik matrix
       pqMatrix(p,q) = 1+p+q; %save number of parameters
     catch
-      disp("warning")
-      loglikliMatrix(p,q)=NaN;
+      disp('Warning')
+      loglikelihood(p,q)=NaN;
       pqMatrix(p,q)=NaN;
     end
   end
 end
 
 
-[~,bic] = aicbic(reshape(loglikliMatrix,maxNoPar^2,1),reshape(pqMatrix,maxNoPar^2,1),numberOfObservations);
-[value,index]=min(bic);
-[pOpt,qOpt]=ind2sub([ maxNoPar],index);
-%% TASK 3
+[~,bic] = aicbic(reshape(loglikelihood,num_params^2,1),reshape(pqMatrix,num_params^2,1),num_obs);
+[~,index]=min(bic);
+[pOpt,qOpt]=ind2sub(num_params,index);
+%% Task 3
 armaOpt=arima(pOpt,0,qOpt);
+armaOpt.Constant = 0;
 armaEstOpt=estimate(armaOpt,returns,'Display','off'); %osäker op detta
 [residuals,condVar]=infer(armaEstOpt,returns); %maybe we shall standardize the residuals? 
 %
@@ -46,36 +48,40 @@ qqplot(residuals);
 figure(2);clf; 
 autocorr(residuals.^2,20);
 %we must allow for hetroscacity and whale-tails! 
-%% TASK4
-maxNoPar=10;
-numberOfObservations=length(residuals);
-loglikliMatrix = zeros(maxNoPar); % Initialize
-pqMatrix = zeros(maxNoPar); %to save number of paramaters
-for q=1:maxNoPar
-  for p=1:maxNoPar
+%% Task 4
+num_params=10;
+num_obs=length(residuals); %Redundant
+garch_loglikelihood = zeros(num_params); % Initialize
+pqMatrix = zeros(num_params); %to save number of paramaters
+
+for q=1:num_params
+  for p=1:num_params
     try
-      mod = garch(p,q); %model struct
-      [fit,~,logL,~] = estimate(mod,residuals,'Display','off');  %fits model
-      loglikliMatrix(p,q) = logL; %save in loglik matrix
+      model = garch(p,q); %model struct
+      [~,~,logL,~] = estimate(mod,residuals,'Display','off');  %fits model
+      garch_loglikelihood(p,q) = logL; %save in loglik matrix
       pqMatrix(p,q) = 1+p+q; %save number of parameters
     catch
-      disp("warning")
-      loglikliMatrix(p,q)=NaN;
+      disp('Warning')
+      garch_loglikelihood(p,q)=NaN;
       pqMatrix(p,q)=NaN;
     end
   end
 end
-[~,bic] = aicbic(reshape(loglikliMatrix,maxNoPar^2,1),reshape(pqMatrix,maxNoPar^2,1),numberOfObservations);
-[value,index]=min(bic);
-[pOpt,qOpt]=ind2sub(maxNoPar,index);
+[~,bic] = aicbic(reshape(garch_loglikelihood,num_params^2,1),reshape(pqMatrix,num_params^2,1),num_obs);
+[~,index]=min(bic);
+[pOpt,qOpt]=ind2sub(num_params,index);
 %%
 garchOpt=garch(pOpt,qOpt);
 garchOptEval=estimate(garchOpt,residuals,'Display','off');
-[resResiduls,~]=infer(garchOptEval,residuals);
-%todo diagnostic check
-%det ser helt miffat ut.
+[var,~]=infer(garchOptEval,residuals); %Computes to conditional variance
 
-
+subplot(1,3,1) %Diagnostics
+plot(residuals./(sqrt(var)))
+subplot(1,3,2)
+qqplot(residuals./(sqrt(var)))
+subplot(1,3,3)
+autocorr(residuals./(sqrt(var)))
 %%
 r=normrnd(0,1,1000,1);
 autocorr(r,20)
